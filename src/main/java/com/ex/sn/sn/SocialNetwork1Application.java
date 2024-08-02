526e7df982e2354c448746139b434db51de14381
mrfriday13th <94086797+mrfriday13th@users.noreply.github.com>
2024-08-02 18:00:10 +0700

    Initial commit

---
+++ src/main/java/com/ex/sn/sn/SocialNetwork1Application.java
@@ -0,0 +1,13 @@
+package com.ex.sn.sn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetwork1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetwork1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ex/sn/sn/SocialNetwork1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.sn.sn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetwork1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
